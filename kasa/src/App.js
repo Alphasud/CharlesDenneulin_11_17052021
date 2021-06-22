ca8dc7aa683507a1b4521a3d3ac6f9422b643a60
charlie <kislapsky@gmail.com>
2021-06-23 01:09:49 +0200

    (fix): minor css fix

--- kasa/src/App.js
+++ kasa/src/App.js
@@ -16,7 +16,7 @@ export default class App extends Component {
 
   componentDidMount() {
     this.setState({ isLoading: true });
-    fetch('http://localhost:3000/hotels.json')
+    fetch('http://192.168.1.31:3000/hotels.json')
       .then(response => {
         if (response.ok) {
           return response.json();
